c7e17d1fbb071059ddaa95c5857b2170753f4c81
Vidit7792 <39307865+Vidit7792@users.noreply.github.com>
2018-07-03 15:17:18 +0530

    Add files via upload

---
+++ javascript4.3.js
@@ -0,0 +1,19 @@
+var evens= [2,4,6,8];
+var odds= [1,3,5,7];
+var primes =[1,3,5,7,11];
+
+var all = [primes,evens,odds];
+var newPrimes = [primes];
+var jamesBond ={
+    first:'James',
+    last:'Bond',
+    country:'United States',
+    twitter:'@jamesbond'
+};
+var{
+    first,
+    last,
+    twitter
+}=jamesBond;
+var players = ['paul','andy','darrell','jim'];
+var [player1,player2,player3,player4]= players;
